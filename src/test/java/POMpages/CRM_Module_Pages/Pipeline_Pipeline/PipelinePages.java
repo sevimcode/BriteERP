2610b796a3a47ba99733afcc1bb9a4de7da50b9e
rana-hamraeva <56229982+rana-hamraeva@users.noreply.github.com>
2020-02-15 22:01:29 -0600

    final code

--- src/test/java/POMpages/CRM_Module_Pages/Pipeline_Pipeline/PipelinePages.java
+++ src/test/java/POMpages/CRM_Module_Pages/Pipeline_Pipeline/PipelinePages.java
@@ -15,4 +15,44 @@ public class PipelinePages {
     public WebElement userNameOnPage;
 
 
+    @FindBy(xpath = "//button[@class='btn btn-primary btn-sm o-kanban-button-new']")
+    public WebElement createButton;
+
+
+
+    @FindBy(xpath = "//input[@class='o_field_char o_field_widget o_input o_required_modifier']")
+    public WebElement opportunityTitleInputBox;
+
+
+    @FindBy(xpath = "//button[@name='close_dialog']")
+    public WebElement createButtonWindowPage;
+
+    @FindBy (xpath = "(//div[@class='oe_kanban_color_0 oe_kanban_global_click o_kanban_record ui-sortable-handle'])[4]")
+    public WebElement createdDeal;
+
+    @FindBy(xpath = "//button[@class='btn btn-sm btn-default o_button_import']")
+    public WebElement importButton;
+
+    @FindBy(xpath = "//span[.='New']")
+    public WebElement newColomn;
+
+    @FindBy(xpath = "//span[.='Qualified']")
+    public WebElement qualifiedColomn;
+    @FindBy(xpath = "//span[.='Approved']")
+    public WebElement approvedColomn;
+    @FindBy(xpath = "//span[.='Proposition']")
+    public WebElement propositionColomn;
+    @FindBy(xpath = "//span[.='Pending']")
+    public WebElement pendingColomn;
+
+    @FindBy (xpath = "//input[@class='o_searchview_input']")
+    public WebElement searchBox;
+
+    @FindBy(xpath = "//div[@class='oe_kanban_content']")
+    public WebElement serchBoxResult;
+
+    @FindBy (xpath = "//div[@class='o_facet_values']")
+    public WebElement pipelineFilter;
+    @FindBy(xpath = "//div[@class='fa fa-sm fa-remove o_facet_remove']")
+    public WebElement mypipelineClosingButton;
 }
