0061b0d545baf4ff0ce3c8e728a80bdbf2b416d1
cardissuer <cardissuer@gmail.com>
2020-02-18 18:05:13 -0600

    18Feb Update

--- src/test/java/POMpages/CRM_Module_Pages/Config_Leads_Oportunities_LostReasons/VerifyingLostReasonsPage.java
+++ src/test/java/POMpages/CRM_Module_Pages/Config_Leads_Oportunities_LostReasons/VerifyingLostReasonsPage.java
@@ -22,21 +22,78 @@ public class VerifyingLostReasonsPage{
         @FindBy (xpath = "//span [@title='Advanced Search...']")
         public WebElement advancedSearch;
 
+        @FindBy (xpath = "//div[@class='btn-group o_search_options']")
+        public WebElement buttonsLine;
+
         @FindBy (xpath = "(//button [@class='o_dropdown_toggler_btn btn btn-sm dropdown-toggle'])[3]")
         public WebElement filtersBtn;
 
-        @FindBy (xpath = "//ul [@class='dropdown-menu o_filters_menu']")
-        public WebElement filtersDropdown;
+        @FindBy (xpath = "//*[text()='Include archived']")
+        public WebElement filtersDropdown1;
 
-        @FindBy (xpath = "(//button [@class='o_dropdown_toggler_btn btn btn-sm dropdown-toggle'])[4]")
+        @FindBy (xpath = "//*[text()='Add Custom Filter']")
+        public WebElement filtersDropdown2;
+
+        @FindBy (xpath = "//div [@class='btn-group hidden-xs o_dropdown']")
         public WebElement groupByBtn;
 
-        @FindBy (xpath = "(//a[@href='#'])[16]")
+        @FindBy (xpath = "//*[text()='Add custom group']")
         public WebElement groupByDropdown;
 
-        @FindBy (xpath = "(//button [@class='o_dropdown_toggler_btn btn btn-sm dropdown-toggle'])[5]")
+        @FindBy (xpath = "(//div [@class='btn-group o_dropdown'])[4]")
         public WebElement favoritesBtn;
 
-        @FindBy (xpath = "//ul [@class='dropdown-menu o_favorites_menu']")
-        public WebElement favoritesDropdown;
+        @FindBy (xpath = "//* [text()='Save current search']")
+        public WebElement favoritesDropdown1;
+
+        @FindBy (xpath = "//* [text()='Add to my Dashboard']")
+        public WebElement favoritesDropdown2;
+
+        @FindBy (xpath = "(//input [@type='checkbox'])[3]")
+        public WebElement nameCheckbox;
+
+        @FindBy (xpath = "(//div[@class='btn-group o_dropdown'])[2]")
+        public WebElement actionBtn;
+
+        @FindBy (xpath = "(//ul[@class='dropdown-menu'])[4]")
+        public WebElement actionDropdownMenu;
+
+        @FindBy (xpath = " //a[contains(text(),'Export')]")
+        public WebElement actionExportBtn;
+
+        @FindBy (xpath = " //a[contains(text(),'Archive')]")
+        public WebElement actionArchiveBtn;
+
+        @FindBy (xpath = " //a[contains(text(),'Unarchive')]")
+        public WebElement actionUnarchiveBtn;
+
+        @FindBy (xpath = " //a[contains(text(),'Delete')]")
+        public WebElement actionDeleteBtn;
+
+        @FindBy (xpath = "//button[@class='btn btn-sm btn-default']")
+        public WebElement nextWindowCloseBtn;
+
+        @FindBy (xpath = "//p[@class='oe_view_nocontent_create']")
+        public WebElement messageOnNewPage;
+
+        @FindBy (xpath = "//*[text()='Ok']")
+        public WebElement okBtn;
+
+        @FindBy (xpath = "//button[@class ='btn btn-primary btn-sm o_list_button_add']")
+        public WebElement createBtn;
+
+        @FindBy (xpath = "//input[@id='o_field_input_127']")
+        public WebElement activeCheckBox;
+
+        @FindBy (xpath = "//input[@class = 'o_field_char o_field_widget o_input o_required_modifier']")
+        public WebElement nameLine;
+
+        @FindBy (xpath = "//button[@class = 'btn btn-primary btn-sm o_form_button_save']")
+        public WebElement saveBtn;
+
+        @FindBy (xpath = "//button[@class = 'btn btn-default btn-sm o_form_button_cancel']")
+        public WebElement discardBtn;
+
+        @FindBy (xpath = "//button[@class = 'btn btn-sm btn-primary']")
+        public WebElement okBtn1;
     }
--- src/test/java/POMtests/CRM_Module_Tests/Config_Leads_Opportuinities_LostReasons_Tests/VerifyingLostReasonsPageTests.java
+++ src/test/java/POMtests/CRM_Module_Tests/Config_Leads_Opportuinities_LostReasons_Tests/VerifyingLostReasonsPageTests.java
@@ -20,20 +20,60 @@ public class VerifyingLostReasonsPageTests {
         verifyingLostReasonsPage.searchInput.sendKeys(Config.getProperty("lostReasonName") + Keys.ENTER);
         verifyingLostReasonsPage.advancedSearch.click();
 
-        Assert.assertTrue(verifyingLostReasonsPage.filtersBtn.isDisplayed()&&verifyingLostReasonsPage.groupByBtn.isDisplayed()&&verifyingLostReasonsPage.favoritesBtn.isDisplayed(),"Buttons aren't displayed on LostReasons page");
+        Assert.assertTrue(verifyingLostReasonsPage.buttonsLine.isDisplayed(),"Buttons aren't displayed on LostReasons page");
 
-//                verifyingLostReasonsPage.filtersBtn.click();
-//                Assert.assertTrue (verifyingLostReasonsPage.filtersDropdown.getText().contains(Config.getProperty("lostReasonsFilterDropdown1")));
-//                Assert.assertTrue (verifyingLostReasonsPage.filtersDropdown.getText().contains(Config.getProperty("lostReasonsFilterDropdown2")));
-//                verifyingLostReasonsPage.filtersBtn.click();
+                verifyingLostReasonsPage.filtersBtn.click();
+                Assert.assertTrue (verifyingLostReasonsPage.filtersDropdown1.getText().contains(Config.getProperty("lostReasonsFilterDropdown1")));
+                Assert.assertTrue (verifyingLostReasonsPage.filtersDropdown2.getText().contains(Config.getProperty("lostReasonsFilterDropdown2")));
 
-                //verifyingLostReasonsPage.groupByBtn.click();
+                verifyingLostReasonsPage.groupByBtn.click();
                 Assert.assertTrue (verifyingLostReasonsPage.groupByDropdown.getText().contains(Config.getProperty("lostReasonsGroupByDropdown")));
-                //verifyingLostReasonsPage.groupByBtn.click();
 
-//                verifyingLostReasonsPage.favoritesBtn.click();
-//                Assert.assertTrue (verifyingLostReasonsPage.favoritesDropdown.getText().contains(Config.getProperty("lostReasonsFavoritesDropdown1")));
-//                Assert.assertTrue (verifyingLostReasonsPage.filtersDropdown.getText().contains(Config.getProperty("lostReasonsFavoritesDropdown2")));
-//                verifyingLostReasonsPage.favoritesBtn.click();
+                verifyingLostReasonsPage.favoritesBtn.click();
+                Assert.assertTrue (verifyingLostReasonsPage.favoritesDropdown1.getText().contains(Config.getProperty("lostReasonsFavoritesDropdown1")));
+                Assert.assertTrue (verifyingLostReasonsPage.favoritesDropdown2.getText().contains(Config.getProperty("lostReasonsFavoritesDropdown2")));
+        }
+
+        @Test
+        public void verifyingNameCheckboxes () {
+                CRMUtils.login();
+                Driver.getDriver().get(Config.getProperty("lostReasonsUrl"));
+                verifyingLostReasonsPage.nameCheckbox.click();
+                verifyingLostReasonsPage.actionBtn.click();
+                //Assert.assertTrue(verifyingLostReasonsPage.actionDropdownMenu.getText().contains(Config.getProperty("lostReasonsActionDropdown1"))&&verifyingLostReasonsPage.actionDropdownMenu.getText().contains(Config.getProperty("lostReasonsActionDropdown2"))&&verifyingLostReasonsPage.actionDropdownMenu.getText().contains("lostReasonsActionDropdown3")&&verifyingLostReasonsPage.actionDropdownMenu.getText().contains("lostReasonsActionDropdown4"));
+                Assert.assertTrue(verifyingLostReasonsPage.actionDropdownMenu.getText().contains(Config.getProperty("lostReasonsActionDropdown1")));
+                Assert.assertTrue(verifyingLostReasonsPage.actionDropdownMenu.getText().contains(Config.getProperty("lostReasonsActionDropdown2")));
+                Assert.assertTrue(verifyingLostReasonsPage.actionDropdownMenu.getText().contains(Config.getProperty("lostReasonsActionDropdown3")));
+                Assert.assertTrue(verifyingLostReasonsPage.actionDropdownMenu.getText().contains(Config.getProperty("lostReasonsActionDropdown4")));
+
+                verifyingLostReasonsPage.actionExportBtn.click();
+                Driver.getDriver().getWindowHandle();
+                verifyingLostReasonsPage.nextWindowCloseBtn.click();
+
+                Driver.getDriver().switchTo().parentFrame();
+                verifyingLostReasonsPage.actionBtn.click();
+                verifyingLostReasonsPage.actionArchiveBtn.click();
+                Assert.assertTrue(verifyingLostReasonsPage.messageOnNewPage.getText().contains("lostReasonsNewPageMessage"));
+
+//                verifyingLostReasonsPage.actionBtn.click();
+//                verifyingLostReasonsPage.actionUnarchiveBtn.click();
+//                Assert.assertTrue(verifyingLostReasonsPage.messageOnNewPage.getText().contains("lostReasonsNewPageMessage"));
+//
+//                verifyingLostReasonsPage.actionBtn.click();
+//                verifyingLostReasonsPage.actionDeleteBtn.click();
+//                verifyingLostReasonsPage.okBtn.click();
+//                Assert.assertTrue(verifyingLostReasonsPage.messageOnNewPage.getText().contains("lostReasonsNewPageMessage"));
+        }
+
+        @Test
+        public void verifyingCreateAndImportBtn () {
+                CRMUtils.login();
+                Driver.getDriver().get(Config.getProperty("lostReasonsUrl"));
+                verifyingLostReasonsPage.createBtn.click();
+                Assert.assertTrue(verifyingLostReasonsPage.activeCheckBox.isSelected());
+                verifyingLostReasonsPage.nameLine.sendKeys("Too expensive");
+                verifyingLostReasonsPage.discardBtn.click();
+                Driver.getDriver().getWindowHandle();
+                verifyingLostReasonsPage.okBtn1.click();
         }
     }
